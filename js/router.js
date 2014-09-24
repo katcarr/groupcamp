3a555e4394d66fee5f8c80d243d59bf584acbe7c
katcarr <kathrynelizabethcarr@gmail.com>
2014-09-24 14:24:45 -0700

    tutorial finshed

--- js/router.js
+++ js/router.js
@@ -30,12 +30,12 @@ Todos.TodosActiveRoute = Ember.Route.extend({
 });
 
 Todos.TodosCompletedRoute = Ember.Route.extend({
-	model: function(){
-		return this.store.filter('todo', function (todo) {
-			return todo.get('isCompleted');
-		});
-	},
-    renderTemplate: function(controller) {
-    	this.render('todos/index', {controller:controller});
-    }
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
+  }
 });
